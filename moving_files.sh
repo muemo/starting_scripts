66a0277bfef9f3be37637ce059749412cee17cb6
muenster <muenster@molx.(none)>
2014-06-27 11:18:42 +0200

    making backups

--- moving_files.sh
+++ moving_files.sh
@@ -2,6 +2,11 @@
 
 echo "mint oder pac?" 
 read ALI
+
+for i in ~/.bash_profile ~/.bash_aliases ~/.bashrc ~/.tmux.conf ~/.vimrc; do 
+	mv ${i} ${i}.backup
+done
+
 cp .bashrc .tmux.conf .vimrc ~/
 cp .bash_aliases_${ALI} ~/.bash_aliases
 echo 'source ~/.bashrc' > ~/.bash_profile
