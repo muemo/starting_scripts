c1a8ee25745e68eb5412ab3f6613b9ac8e28c9f7
muemo <waldlaushaus@freenet.de>
2014-12-07 16:54:58 +0100

    implement inputrc

--- moving_files.sh
+++ moving_files.sh
@@ -3,12 +3,12 @@
 #echo "mint oder pac?" 
 #read ALI
 
-for i in ~/.bash_profile ~/.bashrc ~/.tmux.conf ~/.vimrc; do 
+for i in ~/.bash_profile ~/.bashrc ~/.tmux.conf ~/.vimrc ~/.inputrc; do 
 	if [ -e $i ]; then
 		mv ${i} ${i}.backup
 	fi
 done
 
-cp .bashrc .tmux.conf .vimrc ~/
+cp .bashrc .tmux.conf .vimrc .inputrc ~/
 #cp .bash_aliases_${ALI} ~/.bash_aliases
 echo 'source ~/.bashrc' > ~/.bash_profile
